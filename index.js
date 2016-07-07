6551abecbd6de7e3a886bdef90bf650a744ba8ac
Amila Welihinda <amilajack@users.noreply.github.com>
2016-07-06 22:06:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var nuts = Nuts({
     // GitHub configuration
     repository: 'amilajack/popcorn-time-desktop',
     username: 'spacecow321',
-    token: 'ccd3bfdd1fa187147c9d7d3692ba5d1662677c5f'
+    token: process.env.TOKEN
 });
 
 app.use('/', nuts.router);
